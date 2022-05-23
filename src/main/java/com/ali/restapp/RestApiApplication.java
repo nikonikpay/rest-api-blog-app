b195a0b7a7ca2795c34336a0c5791bdff6389866
nikonikpay <nikonikpay@gmail.com>
2022-05-23 12:50:07 +0200

    deploy on aws

--- src/main/java/com/ali/restapp/RestApiApplication.java
+++ src/main/java/com/ali/restapp/RestApiApplication.java
@@ -1,12 +1,16 @@
 package com.ali.restapp;
 
+import com.ali.restapp.entity.Role;
+import com.ali.restapp.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class RestApiApplication implements CommandLineRunner {
 
     @Bean
     public ModelMapper modelmapper() {
@@ -17,4 +21,18 @@ public class RestApiApplication {
         SpringApplication.run(RestApiApplication.class, args);
     }
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+
+    }
 }
